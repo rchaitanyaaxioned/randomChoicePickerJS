99d2246442db32c86f532d32e5b3c1e571d5c899
rchaitanyaaxioned <chaitanyar@axioned.com>
2022-07-08 12:46:22 +0530

    Before adding class to textarea in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,20 @@
-/* Author: 
+var inputDiv = document.querySelector(".input");
+var textArea = document.getElementsByTagName("textarea");
 
-*/
+textArea.onkeydown = function (event) {
+  alert("Fun Called!")
+  var eventKey = event.key;
+  console.log("eventKey : " + eventKey);
+}
 
 
+// function (event) {
+//   var keycode = event.keyCode;
 
-
+//   if (keycode === 38) {
+//     moveUp();
+//   }
+// }
 
 
 
