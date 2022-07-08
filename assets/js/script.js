b3ec45d9c5a580a44a61b0ef37b63576a30814be
rchaitanyaaxioned <chaitanyar@axioned.com>
2022-07-08 19:29:39 +0530

    Feature-js code with span problem

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,42 @@
 var inputDiv = document.querySelector(".input");
 var textArea = document.querySelector(".text-area");
 var outputDiv = document.querySelector(".output");
+var newStr;
 
-textArea.onkeydown = function (event) {
-  var eventKeyCode = event.which;
-  if (eventKeyCode === 13) {
-    let textContent = textArea.value;
-    stringSplit(textContent);
-  }
+textArea.addEventListener('keypress', onKeyPress);
+
+function onKeyPress() {
+  var tAContent = textArea.value;
+  // var newStr = "";
+  // newStr = tAContent;
+  // console.log(newStr);
+
+  // var splitStr = tAContent.split(",");
+  // console.log("splitStr : " + splitStr);
+
+  var inputArr = tAContent.split(",");
+
+  // console.log(inputArr);
+
+  stringSplit(inputArr);
+
+  // if (eventKeyCode === 13) {
+  //   stringSplit(inputArr);
+  // }
 }
 
-function stringSplit(textContent) {
-  const inputArr = textContent.split(",");
-  // outputDiv.innerHTML = inputArr;
+function stringSplit(arr) {
 
-  inputArr.forEach(arrIterator)
+  arr.forEach(arrIterator);
 
-  function arrIterator(item, index) {
+  function arrIterator(item, index, array) {
+    // console.log(item + "," + index);
+
+    //let newItem = array[index];
+
     let span = document.createElement("span");
     span.innerText = item;
     outputDiv.appendChild(span);
-    //console.log("item : " + item + "," + "index : " + index);
   }
 }
 
