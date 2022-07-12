6ea3544392b630255398df31836c72b73f0cbb86
rchaitanyaaxioned <chaitanyar@axioned.com>
2022-07-12 12:10:33 +0530

    FIX: starting space is removed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,23 @@
 var inputDiv = document.querySelector(".input");
 var textArea = document.querySelector(".text-area");
 var outputDiv = document.querySelector(".output");
-var newStr;
 
 textArea.addEventListener('keypress', onKeyPress);
 
 function onKeyPress() {
   let txtContent = textArea.value.trim();
-
-  const splitArr = txtContent.split(",");
-
-  stringSplit(splitArr);
+  if (txtContent != "") {
+    const splitArr = txtContent.split(",");
+    stringSplit(splitArr);
+  }
 }
 
 function stringSplit(arr) {
-
   outputDiv.innerHTML = "";
-
   arr.forEach(arrIterator);
-
-  function arrIterator(item, index, array) {
+  function arrIterator(item) {
     let span = document.createElement("span");
     span.innerText = item.trim();
     outputDiv.appendChild(span);
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
